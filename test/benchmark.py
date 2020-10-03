b03f7bb233e073cd5e1e5b1aafe1ae271afff982
Hannes Perrot <hp96@gmx.de>
2020-10-03 20:47:40 +0200

    Add minimal benchmark

---
+++ test/benchmark.py
@@ -0,0 +1,29 @@
+import torch
+import cpp.splatting_cpp
+import timeit
+
+
+def run_test(batch_size, spatial_size, flow_init):
+    frame = torch.ones(batch_size, 3, spatial_size, spatial_size)
+    if flow_init == "zeros":
+        flow = torch.zeros(batch_size, 2, spatial_size, spatial_size)
+    elif flow_init == "ones":
+        flow = torch.ones(batch_size, 2, spatial_size, spatial_size)
+    else:
+        raise Exception
+    ex_time = timeit.timeit(
+        lambda: cpp.splatting_cpp.SplattingFunction.apply(frame, flow),
+        number=1,
+    )
+    print(f"{batch_size=}\t{spatial_size=}\t{flow_init=}\t{ex_time=}")
+
+
+def benchmark():
+    for batch_size in [1, 2, 4]:
+        for spatial_size in [2, 8, 16]:
+            for flow_init in ["zeros", "ones"]:
+                run_test(batch_size, spatial_size, flow_init)
+
+
+if __name__ == "__main__":
+    benchmark()
