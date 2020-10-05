484628a2cda357b1b537c5b3be4fc23988d321fe
Hannes Perrot <hp96@gmx.de>
2020-10-05 12:03:56 +0200

    Add assertions in python function

--- cpp/splatting_cpp.py
+++ cpp/splatting_cpp.py
@@ -7,6 +7,12 @@ class SplattingFunction(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, frame, flow):
+        assert(frame.size()[0] == flow.size()[0])
+        assert(frame.size()[2] == flow.size()[2])
+        assert(frame.size()[3] == flow.size()[3])
+        assert(flow.size()[1] == 2)
+        assert(frame.is_contiguous())
+        assert(flow.is_contiguous())
         ctx.save_for_backward(frame, flow)
         output = splatting_cpp.splatting_forward(frame, flow)
         return output
