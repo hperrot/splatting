9a62d462bd71875ad0b1743b70315b7c58d7bd93
Hannes Perrot <hp96@gmx.de>
2020-10-05 13:46:01 +0200

    Improve benchmark

--- test/benchmark.py
+++ test/benchmark.py
@@ -3,7 +3,7 @@ import cpp.splatting_cpp
 import timeit
 
 
-def run_test(batch_size, spatial_size, flow_init):
+def run_test(batch_size, spatial_size, flow_init, repetitions=10):
     frame = torch.ones(batch_size, 3, spatial_size, spatial_size)
     if flow_init == "zeros":
         flow = torch.zeros(batch_size, 2, spatial_size, spatial_size)
@@ -13,8 +13,8 @@ def run_test(batch_size, spatial_size, flow_init):
         raise Exception
     ex_time = timeit.timeit(
         lambda: cpp.splatting_cpp.SplattingFunction.apply(frame, flow),
-        number=1,
-    )
+        number=repetitions,
+    ) / repetitions
     print(f"{batch_size=}\t{spatial_size=}\t{flow_init=}\t{ex_time=}")
 
 
