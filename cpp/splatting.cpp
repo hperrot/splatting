7cae7234fcbb711b8c4320d242642e69f3102b7c
Hannes Perrot <hp96@gmx.de>
2020-10-05 11:36:18 +0200

    Precompute bounds

--- cpp/splatting.cpp
+++ cpp/splatting.cpp
@@ -1,7 +1,5 @@
 #include <torch/extension.h>
-
 #include <vector>
-
 #include <cmath>
 
 
@@ -10,20 +8,24 @@ torch::Tensor splatting_forward_cpu_impl(
     const torch::Tensor frame,
     const torch::Tensor flow
 ) {
-    // init output
     torch::Tensor output = torch::zeros_like(frame);
-    // create accessors
+
     auto frame_a = frame.accessor<scalar_t, 4>();
     auto flow_a = flow.accessor<scalar_t, 4>();
     auto output_a = output.accessor<scalar_t, 4>();
-    // actual splatting
-    for (auto N=0; N<frame.size(0); N++){
-        for (auto C=0; C<frame.size(1); C++) {
-            for(auto Y=0; Y<frame.size(2); Y++) {
-                for(auto X=0; X<frame.size(3); X++) {
 
-                    const float outputX = (float) X + flow_a[N][0][Y][X];
-                    const float outputY = (float) Y + flow_a[N][1][Y][X];
+    const auto N = frame.size(0);
+    const auto C = frame.size(1);
+    const auto Y = frame.size(2);
+    const auto X = frame.size(3);
+
+    for (auto n=0; n<N; n++){
+        for (auto c=0; c<C; c++) {
+            for(auto y=0; y<Y; y++) {
+                for(auto x=0; x<X; x++) {
+
+                    const float outputX = (float) x + flow_a[n][0][y][x];
+                    const float outputY = (float) y + flow_a[n][1][y][x];
 
                     const auto northwestX = (int) (floor(outputX));
                     const auto northwestY = (int) (floor(outputY));
@@ -39,22 +41,24 @@ torch::Tensor splatting_forward_cpu_impl(
                     const auto southwest = ((float) (northeastX) - outputX   ) * (outputY    - (float) (northeastY));
                     const auto southeast = (outputX    - (float) (northwestX)) * (outputY    - (float) (northwestY));
 
-                    if ((northwestX >= 0) && (northwestX < output.size(3)) && (northwestY >= 0) && (northwestY < output.size(2))) {
-                        output_a[N][C][northwestY][northwestX] += northwest * frame_a[N][C][Y][X];
+                    if ((northwestX >= 0) && (northwestX < X) && (northwestY >= 0) && (northwestY < Y)) {
+                        output_a[n][c][northwestY][northwestX] += northwest * frame_a[n][c][y][x];
                     }
-                    if ((northeastX >= 0) && (northeastX < output.size(3)) && (northeastY >= 0) && (northeastY < output.size(2))) {
-                        output_a[N][C][northeastY][northeastX] += northeast * frame_a[N][C][Y][X];
+                    if ((northeastX >= 0) && (northeastX < X) && (northeastY >= 0) && (northeastY < Y)) {
+                        output_a[n][c][northeastY][northeastX] += northeast * frame_a[n][c][y][x];
                     }
-                    if ((southwestX >= 0) && (southwestX < output.size(3)) && (southwestY >= 0) && (southwestY < output.size(2))) {
-                        output_a[N][C][southwestY][southwestX] += southwest * frame_a[N][C][Y][X];
+                    if ((southwestX >= 0) && (southwestX < X) && (southwestY >= 0) && (southwestY < Y)) {
+                        output_a[n][c][southwestY][southwestX] += southwest * frame_a[n][c][y][x];
                     }
-                    if ((southeastX >= 0) && (southeastX < output.size(3)) && (southeastY >= 0) && (southeastY < output.size(2))) {
-                        output_a[N][C][southeastY][southeastX] += southeast * frame_a[N][C][Y][X];
+                    if ((southeastX >= 0) && (southeastX < X) && (southeastY >= 0) && (southeastY < Y)) {
+                        output_a[n][c][southeastY][southeastX] += southeast * frame_a[n][c][y][x];
                     }
+
                 }
             }
         }
     }
+
     return output;
 }
 
